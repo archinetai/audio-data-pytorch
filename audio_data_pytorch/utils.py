288b474055737d63d4170019705dfd3f7ced046e
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-08-19 11:32:04 +0200

    feat: add fractional random split util

--- audio_data_pytorch/utils.py
+++ audio_data_pytorch/utils.py
@@ -1,6 +1,9 @@
+import math
 import re
-from typing import Optional, TypeVar
+from typing import List, Optional, Sequence, TypeVar
 
+import torch
+from torch.utils.data.dataset import Dataset, Subset
 from typing_extensions import TypeGuard
 
 T = TypeVar("T")
@@ -13,3 +16,22 @@ def exists(val: Optional[T]) -> TypeGuard[T]:
 def camel_to_snake(name: str) -> str:
     name = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
     return re.sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()
+
+
+def fractional_random_split(
+    dataset: Dataset[T], fractions: Sequence[int]
+) -> List[Subset[T]]:
+    """Fractional split that follows the same convention as random_split"""
+    assert sum(fractions) == 1.0, "Fractions must sum to 1.0"
+
+    length = len(dataset)  # type: ignore[arg-type]
+    indices = torch.randperm(length)
+    splits = []
+    cursor = 0
+
+    for fraction in fractions:
+        next_cursor = math.ceil(length * fraction + cursor)
+        splits += [Subset(dataset, indices[cursor:next_cursor])]  # type: ignore[arg-type] # noqa
+        cursor = next_cursor
+
+    return splits
